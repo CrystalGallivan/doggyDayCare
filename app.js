52259e19b95579c2e1223c4f5d30c6715894ec17
Your Name <crystalgallivan@gmail.com>
2019-05-09 12:58:58 -0600

    html bones - staring java

---
+++ app.js
@@ -0,0 +1,11 @@
+let dog = {
+  name: "Mr. Kibbles",
+  moods: ["sad", "tail up", "playful", 'happy'],
+  pets: 0,
+  moodIndex: 0,
+  satisfaction: 5,
+  images: ["/assets/saddog.jpg", "/assets/tailup.jpg", "/assets/playful.jpg", "/assets/happydog.jpg"],
+
+}
+
+function pet()
